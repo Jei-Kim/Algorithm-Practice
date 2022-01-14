4bf70ed8beeceddff95567e95557d2d0724a495d
Jei-Kim <jeje2kim@gmail.com>
2022-01-14 21:20:14 +0900

    .

---
+++ inflearn-java/src/section01/Main_06.java
@@ -0,0 +1,21 @@
+package section01;
+
+import java.util.*;
+
+class Main_06 {	
+	public String solution(String str){
+		String answer="";
+		for(int i=0; i<str.length(); i++){
+			//System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
+			if(str.indexOf(str.charAt(i))==i) answer+=str.charAt(i);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main_06 T = new Main_06();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}

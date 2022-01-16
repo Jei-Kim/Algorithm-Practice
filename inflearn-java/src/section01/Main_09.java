ef0b13a1f661bc3c5d1a0637db7a2651f18a17f9
Jei-Kim <jeje2kim@gmail.com>
2022-01-16 23:12:54 +0900

    .

---
+++ inflearn-java/src/section01/Main_09.java
@@ -0,0 +1,25 @@
+package section01;
+
+import java.util.*;
+
+class Main_09 {	
+	public int solution(String s){
+		//int answer=0;
+		String answer="";
+		for(char x : s.toCharArray()){
+			//if(x>=48 && x<=57) answer=answer*10+(x-48);
+			/*if(Character.isDigit(x)){
+				answer=answer*10+ Character.getNumericValue(x);
+			}*/
+			if(Character.isDigit(x)) answer+=x;
+		}
+		return Integer.parseInt(answer);
+	}
+
+	public static void main(String[] args){
+		Main_09 T = new Main_09();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}

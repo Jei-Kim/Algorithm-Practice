0f55688075c71870ce2022b862c423dcd8a1276d
Jei-Kim <jeje2kim@gmail.com>
2022-02-24 19:38:08 +0900

    .

--- inflearn-java/src/section03/Main_04.java
+++ inflearn-java/src/section03/Main_04.java
@@ -17,14 +17,17 @@ class Main_04 {
 	}
 
 	public static void main(String[] args){
+
 		Main_04 T = new Main_04();
 		Scanner kb = new Scanner(System.in);
 		int n=kb.nextInt();
 		int m=kb.nextInt();
 		int[] arr=new int[n];
+
 		for(int i=0; i<n; i++){
 			arr[i]=kb.nextInt();
 		}
+        
 		System.out.print(T.solution(n, m, arr));
 	}
 }

073cb7e64fcfdb7da7d27e77b01c13259afb60a7
Jei-Kim <jeje2kim@gmail.com>
2022-03-20 20:39:17 +0900

    .

---
+++ inflearn-java/src/section05/Main05.java
@@ -0,0 +1,25 @@
+package section05;
+
+import java.util.*;
+
+class Main05 {	
+	public int solution(String str){
+		int cnt=0;
+		Stack<Character> stack=new Stack<>();
+		for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)=='(') stack.push('(');
+			else{
+				stack.pop();
+				if(str.charAt(i-1)=='(') cnt+=stack.size();
+				else cnt++;
+			}
+		}
+		return cnt;
+	}
+	public static void main(String[] args){
+		Main05 T = new Main05();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.println(T.solution(str));
+	}
+}

4e9b6dc001ff17d49d3903c7d685e63a660aa557
Jei-Kim <jeje2kim@gmail.com>
2022-04-06 19:53:39 +0900

    .

---
+++ inflearn-java/src/section06/Main06.java
@@ -0,0 +1,24 @@
+package section06;
+import java.util.*;
+
+class Main06 {	
+    
+	public ArrayList<Integer> solution(int n, int[] arr){
+		ArrayList<Integer> answer=new ArrayList<>();
+		int[] tmp=arr.clone();
+		Arrays.sort(tmp);
+		for(int i=0; i<n; i++){
+			if(arr[i]!=tmp[i]) answer.add(i+1);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main06 T = new Main06();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+		for(int x : T.solution(n, arr)) System.out.print(x+" ");
+	}
+}

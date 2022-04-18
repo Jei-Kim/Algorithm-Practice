59b988cb035432b5463dce50797b7f79e58311aa
Jei-Kim <jeje2kim@gmail.com>
2022-04-19 07:41:51 +0900

    .

---
+++ inflearn-java/src/section07/Main03.java
@@ -0,0 +1,13 @@
+package section07;
+import java.util.*;
+
+class Main03 {
+	public int DFS(int n){
+		if(n==1) return 1;
+		else return n*DFS(n-1);
+	}
+	public static void main(String[] args){
+		Main03 T = new Main03();
+		System.out.println(T.DFS(5));
+	}	
+}

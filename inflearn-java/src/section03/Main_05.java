16821a12f13c9de8b264e141a9d789d91e60b0a5
Jei-Kim <jeje2kim@gmail.com>
2022-05-28 17:46:10 +0900

    .

--- inflearn-java/src/section03/Main_05.java
+++ inflearn-java/src/section03/Main_05.java
@@ -1,6 +1,12 @@
 package section03;
 import java.util.*;
 
+/*
+5. 연속된 자연수의 합
+N입력으로 양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는
+방법의 가짓수를 출력하는 프로그램을 작성
+*/
+
 class Main_05 {	
 	public int solution(int n){
 		int answer=0, sum=0;

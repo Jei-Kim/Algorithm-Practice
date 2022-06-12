7fd534a9be178f635e3d2ea6d4f49066380a9a14
Jei-Kim <jeje2kim@gmail.com>
2022-06-12 21:16:43 +0900

    .

--- inflearn-java/src/section04/Main_02.java
+++ inflearn-java/src/section04/Main_02.java
@@ -1,6 +1,12 @@
 package section04;
 import java.util.*;
 
+/*
+2. 아나그램(해쉬)
+길이가 같은 두 개의 단어가 주어지면 두 단어가 아나그램인지 판별하는 프로그램을 작성하세
+요. 아나그램 판별시 대소문자가 구분됩니다.
+*/
+
 class Main_02 {	
 	public String solution(String s1, String s2){
 		String answer="YES";
--- inflearn-java/src/section04/Main_03.java
+++ inflearn-java/src/section04/Main_03.java
@@ -1,6 +1,12 @@
 package section04;
 import java.util.*;
 
+/*
+3. 매출액의 종류
+- N일간의 매출기록과 연속구간의 길이 K가 주어지면 첫 번째 구간부터 각 구간별 매출액의 종
+류를 출력하는 프로그램을 작성
+*/
+
 class Main_03 {	
 	public ArrayList<Integer> solution(int n, int k, int[] arr){
 		ArrayList<Integer> answer = new ArrayList<>();

8f08b33132b2423878b853832d5a4e0208ff3747
Jei-Kim <jeje2kim@gmail.com>
2022-08-22 23:02:14 +0900

    .

---
+++ js/basic09.js
@@ -0,0 +1,46 @@
+/* 9. 함수 표현식, 화살표 함수*/
+
+//함수 표현식 -인터프리터
+showError();
+let showError = function () {
+    console.log('error');
+} // 오류 -> 함수 사용 전 선언해줘야 하기 때문
+
+// 함수 선언문 -호이스팅
+showError();
+function showError() {
+    console.log('error');
+}
+
+//화살표 함수
+let showError = () => {
+    console.log('error');
+}
+
+
+//화살표 함수2
+
+//변경 전
+const sayHello = function (name) {
+    const msg = 'Hello, ${name}';
+    console.log(msg);
+};
+
+//변경 후
+const sayHello = (name) => {
+    const msg = 'Hello, ${name}';
+    console.log(msg);
+};
+
+//변경 전
+const add = function (num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+//변경 후
+const add = (num1, num2) => {
+    //return num1 + num2; //return문 한줄로 줄이기
+    num1 + num2; //return 생략
+}
+

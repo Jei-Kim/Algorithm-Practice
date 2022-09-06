c4eaff6e3ece6604cdf8d0490512d7d7c464e1a7
Jei-Kim <jeje2kim@gmail.com>
2022-09-06 22:07:07 +0900

    .

--- js/ajax01.js
+++ js/ajax01.js
@@ -1,14 +1,16 @@
 $ajax({
-    type: "post",
-    contentType: "application/json",
+    type: "post", //post,get,delete,put
+    contentType: "application/json", //전송할 데이터 타입
     url: "",
+    async: "true", //true:비동기 / false:동기(응답이 있을 때까지 기다림)
     data: JSON.stringify(searchData),
-    dataType: "json",
-    timeout: 100000,
-    success: function (data) {
-
+    dataType: "json", //서버에서 반환되는 데이터 타입
+    timeout: 100000, //제한시간(밀리초)
+    chache: false, //기본값 false
+    success: function (data) { //통신 성공 시 실행되는 ajax event
+        console.log(data.d); //.net에서는 data.d로 읽어옴
     },
-    error: function (error) {
+    error: function (error) { // 통신 실패 시 실행되는 ajax event
         console.log(error);
     }
 });
